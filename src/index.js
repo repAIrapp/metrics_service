2c0297905810a42177b0932d92cfc3579727b255
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-08-21 15:59:50 +0200

    chore(ci): add basic CI workflow

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const client = require("prom-client");
 
 const app = express();
+const helmet = require("helmet");
 const port = 9100; // port que prometheus va scraper
 
 // pour créer un registre et quelques métriques
@@ -17,6 +18,7 @@ register.registerMetric(httpRequestCounter);
 client.collectDefaultMetrics({ register });
 
 // Middleware pour enregistrer les métriques
+app.use(helmet());
 app.use((req, res, next) => {
   res.on("finish", () => {
     httpRequestCounter.inc({
@@ -37,6 +39,18 @@ app.get("/metrics", async (req, res) => {
   res.setHeader("Content-Type", register.contentType);
   res.send(await register.metrics());
 });
+// app.get("/metrics", async (req, res) => {
+//   const auth = req.headers.authorization;
+//   const expected = `Bearer ${process.env.METRICS_TOKEN}`;
+
+//   if (auth !== expected) {
+//     return res.status(403).send("Forbidden");
+//   }
+
+//   res.setHeader("Content-Type", register.contentType);
+//   res.send(await register.metrics());
+// });
+
 
 app.listen(port, () => {
   console.log(`Metrics service listening on http://localhost:${port}/metrics`);
